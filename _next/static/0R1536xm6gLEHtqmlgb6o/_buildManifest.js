e50e6fd0ea33ccc914a02166d109980064d01e7a
wenty-l <wenty.l@binance.com>
2025-06-19 17:16:48 +0800

    Updates

--- _next/static/0R1536xm6gLEHtqmlgb6o/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-218dda0dbe8084f3.js"],"/404":["static/chunks/pages/404-95afc910ade4980a.js"],"/_error":["static/chunks/pages/_error-49cda0c38976f66a.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
